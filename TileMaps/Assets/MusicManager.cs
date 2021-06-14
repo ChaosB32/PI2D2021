4b7aac5ee2307f6f65041af5140d3a24b5be17f4
Renato Carvalho <renatocarvalhojunior97@gmail.com>
2021-06-13 21:25:25 -0300

    BuildUltimaEntrega
    Melhorias tiles animados
    Ajustes para a ultima entrega

--- TileMaps/Assets/MusicManager.cs
+++ TileMaps/Assets/MusicManager.cs
@@ -26,7 +26,7 @@ public class MusicManager : MonoBehaviour
     public void Play(AudioClip musicToPlay)
     {
         audioSource.clip = musicToPlay;
-        audioSource.volume = 0.3f;
+        audioSource.volume = 0.2f;
         audioSource.Play();
     }
     public void PlayP(AudioClip musicToPlay)
